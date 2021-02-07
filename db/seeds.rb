c512e943f8d65a11ff74d3f6c70a9ba04796628e
akilchomoko <akil@chomoko.com>
2021-02-07 20:09:15 +0000

    db:seed for 1982

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,77 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 # TODO: add 50 titles from between 1977 and 1998 from http://www.omdbapi.com/
+
+require 'uri'
+require 'net/http'
+require 'openssl'
+require 'json'
+
+
+def genre_resolve(name)
+  name = "Unknown" unless name
+  genre = Genre.find_by(name: name)
+  if !genre
+    genre = Genre.create(name: name)
+  end
+  genre
+end
+
+def get_imdb_data(url)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(url)
+  request["x-rapidapi-key"] = 'ee4080f3cemshab69ec68f2c3a2dp11fc01jsn6726b24ce3bb'
+  request["x-rapidapi-host"] = 'movies-tvshows-data-imdb.p.rapidapi.com'
+
+  response = http.request(request)
+  JSON.parse(response.body)
+end
+
+
+puts "Deleting all title and genres ..."
+Title.delete_all
+Genre.delete_all
+
+year = "1982"
+recs = 20
+total = 0
+puts "Calling IMDB for year #{year}, #{recs} at a time"
+
+page = 1
+
+loop do
+  url = URI("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-byyear&page=#{page}&year=#{year}")
+  response_hash = get_imdb_data(url)
+  count = response_hash["results"]
+  total_count = response_hash["Total_results"].to_i
+  puts "#{(recs * (page - 1))+1} .. #{((recs * (page - 1) + count)+1} records of #{total_count} from IMDB received"
+
+  for i in 0..(count-1) do
+    title = Title.new
+
+    title.title = response_hash["movie_results"][i]["title"]
+    imdb_id = response_hash["movie_results"][i]["imdb_id"]
+
+    url_by_imdb_id = URI("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movie-details&imdb=#{imdb_id}")
+    details_hash = get_imdb_data(url_by_imdb_id)
+
+    title.description = details_hash["description"]
+    title.release_year = details_hash["year"]
+    title.genre = genre_resolve(details_hash["genres"][0])
+    title.rate_per_day = details_hash["imdb_rating"].to_f * 100
+
+    title.save
+    total =+ 1
+    puts "#{total}. #{title.title} rated #{details_hash["imdb_rating"]}, to rent for #{title.rate_per_day}p/day"
+  end
+
+  page > total_count/recs ? break : page += 1
+  break if total > 600
+
+end
+
+
+
