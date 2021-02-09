48d0987b3529298e7a66aaa418b84d8317ed01a6
rachel herlihy <rachellouise.herlihy@gmail.com>
2021-02-09 21:04:04 +0000

    update on the creation of rentals

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :rentals
+  has_many :rentals, dependent: :destroy
 end
