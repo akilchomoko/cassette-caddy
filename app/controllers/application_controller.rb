ecd65eb977bb35b96826645b7b6c44b16fd3c725
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-02-09 21:35:01 +0000

    add todo to beef up signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # TODO: add additional parameters to signup page
+  # see https://kitt.lewagon.com/camps/476/lectures/05-Rails%2F06-Airbnb-Devise#/0/5/3
 end
