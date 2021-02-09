8b51fca471935c059559e461592411ce4edbe564
akilchomoko <akil@chomoko.com>
2021-02-09 21:55:35 +0000

    Completed title#show and title methods for idbm.details and imdb.poster api data

--- app/controllers/titles_controller.rb
+++ app/controllers/titles_controller.rb
@@ -1,5 +1,14 @@
 class TitlesController < ApplicationController
-    def index
-        @title = Title.all
-    end
+  # Title controller
+
+  def index
+    @title = Title.all
+  end
+
+  def show
+    # refactored here, as we don't have edit, new, or others. Removed before_action too.
+    @title = Title.find(params[:id])
+    @title_details = @title.imdb_details
+    @title_poster = @title.imdb_poster
+  end
 end
--- app/models/title.rb
+++ app/models/title.rb
@@ -3,4 +3,34 @@ class Title < ApplicationRecord
   has_many :rentals
   has_many :users, through: :rental
   validates :title, presence: true
+
+  require 'uri'
+  require 'net/http'
+  require 'openssl'
+  require 'json'
+
+  def imdb_details
+    url = URI("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movie-details&imdb=#{imdb_id}")
+    get_imdb_data(url)
+  end
+
+  def imdb_poster
+    url = URI("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-movies-images-by-imdb&imdb=#{imdb_id}")
+    get_imdb_data(url)
+  end
+
+  private
+
+  def get_imdb_data(url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+    request["x-rapidapi-key"] = ENV.fetch('RAPID_IMDB_API_KEY')
+    request["x-rapidapi-host"] = 'movies-tvshows-data-imdb.p.rapidapi.com'
+
+    response = http.request(request)
+    JSON.parse(response.body)
+  end
 end
