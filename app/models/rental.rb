2137b557d25e8a43ec0a7c38c50f341d97173ae7
akilchomoko <akil@chomoko.com>
2021-02-13 12:44:51 +0000

    Rented / Already Rented

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -4,4 +4,8 @@ class Rental < ApplicationRecord
   validates :user_id, presence: true
   validates :title_id, presence: true
   validates_uniqueness_of :title_id, scope: [:user_id, :start_date]
+
+  def self.is_rented(title, user)
+    Rental.find_by(user_id: user.id, title_id: title.id, end_date: nil)
+  end
 end
