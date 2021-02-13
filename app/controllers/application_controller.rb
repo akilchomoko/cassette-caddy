9882c0b9bc222b88250ec55b671c3e812b0076ba
Gareth Maddock <9137502+gtm19@users.noreply.github.com>
2021-02-13 13:55:02 +0000

    finish user account info/edit/update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :address1, :address2, :postcode, :phonenumber])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :address1, :address2, :postcode, :phonenumber])
   end
 end
