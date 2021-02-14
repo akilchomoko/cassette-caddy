551e00f9e691ff991908fabf4ea3a5fa149a397d
akilchomoko <akil@chomoko.com>
2021-02-14 17:45:29 +0000

    Pops Up Added

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -11,8 +11,10 @@ const buildMap = (mapElement) => {
 
 const placeMarkers = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup) // add this
       .addTo(map);
   });
 };
