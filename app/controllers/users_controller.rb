668ebce36edc927b643e8e6730206bdd3fda5ad9
akilchomoko <akil@chomoko.com>
2021-02-14 15:13:07 +0000

    Added user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,16 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  def index
+    @users = User.all
+
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @users.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :rememberable, :validatable
 
   has_many :rentals, dependent: :destroy
+
+  def address
+    [address1, address2, postcode].compact.join(', ')
+  end
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address1?
 end
