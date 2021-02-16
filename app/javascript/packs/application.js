adcf2d76f619860539345ee1aa154838a414948a
rachel herlihy <rachellouise.herlihy@gmail.com>
2021-02-16 22:08:48 +0000

    creation of the carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,14 +28,41 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
-document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-});
-
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+
+  $('#carouselExample').on('slide.bs.carousel', function (e) {
+
+    /*
+
+    CC 2.0 License Iatek LLC 2018
+    Attribution required
+
+    */
+
+    var $e = $(e.relatedTarget);
+
+    var idx = $e.index();
+    console.log("IDX :  " + idx);
+
+    var itemsPerSlide = 8;
+    var totalItems = $('.carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+})();
 })
+
