7d13a01f3b231ff08f4e42480637ff1a9db09063
akilchomoko <akil@chomoko.com>
2021-02-19 20:58:12 +0000

    Type writer effect 50% fixed

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -35,7 +35,7 @@ const placeMarkers = (map, markers) => {
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 5000 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 3000 });
 };
 
 const initMapbox = () => {
